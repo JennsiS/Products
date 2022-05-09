c163b8618aeba1853dd5958992765accf1bf3091
Luis Quezada <que18028@uvg.edu.gt>
2022-05-08 21:38:23 -0600

    Update Catalogo.jsx

--- ecommerce-uvg/ecommerce-frontend/src/components/Catalogo.jsx
+++ ecommerce-uvg/ecommerce-frontend/src/components/Catalogo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 
-export function Catalogo() {
+export default function Catalogo() {
   return (
     <>
       <div className="catalogo">
