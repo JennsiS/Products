8957f9b140fd73947ef22b6eb0cc98d901044eb6
Luis Quezada <que18028@uvg.edu.gt>
2022-05-08 21:56:36 -0600

    First card functional

--- ecommerce-uvg/ecommerce-frontend/src/components/Catalogo.jsx
+++ ecommerce-uvg/ecommerce-frontend/src/components/Catalogo.jsx
@@ -1,5 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Catalogo.scss'
+import prod1 from "../imgs/phone_1.jpg"
 
 export default function Catalogo() {
   return (
@@ -7,6 +12,14 @@ export default function Catalogo() {
       <div className="catalogo">
         <div id=""></div>
         <Button>PROBANDO</Button>
+        <div className="card" alt = "prueba">
+          <img className="card-img-top" src={prod1} alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>      
     </>
   );
