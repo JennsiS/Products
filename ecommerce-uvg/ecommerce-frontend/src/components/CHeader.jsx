cab410c635bfeba6e3f68f97a222f3753847a773
Javier Salazar <javierandre117@gmail.com>
2022-05-10 19:18:49 -0600

    probando cosas

--- ecommerce-uvg/ecommerce-frontend/src/components/CHeader.jsx
+++ ecommerce-uvg/ecommerce-frontend/src/components/CHeader.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react"
-import "./CHeader.scss"
-import logo from "../imgs/logo.png"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "../styles/CHeader.scss";
+import logo from "../imgs/logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { FaGripHorizontal } from "react-icons/fa";
 
 export default class CHeader extends React.Component {
@@ -13,18 +13,35 @@ export default class CHeader extends React.Component {
           <img id="catalogo_logo" src={logo} alt="" />
           <div id="spacer_div" />
           <ul className="nav_list">
-            <button type="button" id="nav_button"> Home </button>
-            <button type="button" id="nav_button"> Browse </button>
-            <button type="button" id="nav_button"> Menu </button>
-            <button type="button" id="nav_button"> Products </button>
+            <button type="button" id="nav_button">
+              {" "}
+              Home{" "}
+            </button>
+            <button type="button" id="nav_button">
+              {" "}
+              Browse{" "}
+            </button>
+            <button type="button" id="nav_button">
+              {" "}
+              Menu{" "}
+            </button>
+            <button type="button" id="nav_button">
+              {" "}
+              Products{" "}
+            </button>
           </ul>
           <div className="__bar" />
           <ul className="nav_list">
-            <button type="button" id="nav_button"> Contact </button>
+            <button type="button" id="nav_button">
+              {" "}
+              Contact{" "}
+            </button>
           </ul>
-          <a href="#" className="btn btn-primary" id="btn-menu"><FaGripHorizontal/></a>
+          <a href="#" className="btn btn-primary" id="btn-menu">
+            <FaGripHorizontal />
+          </a>
         </nav>
       </header>
-    )
+    );
   }
 }
