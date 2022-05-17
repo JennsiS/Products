b4963ed5f7071311bd5df0bf3435b6dc54b6136c
Nate20029 <70552363+Nate20029@users.noreply.github.com>
2022-05-17 17:50:27 -0600

    Primera implementacion de Search Bar

--- ecommerce-uvg/ecommerce-frontend/src/components/Catalogo.jsx
+++ ecommerce-uvg/ecommerce-frontend/src/components/Catalogo.jsx
@@ -10,6 +10,8 @@ import prod2 from "../imgs/prod_2.png"
 import prod3 from "../imgs/prod_3.png"
 import prod4 from "../imgs/prod_4.png"
 
+
+
 export default function Catalogo() {
   const [products, setProducts] = useState([
     { id:0 , nombre: 'Samsung S5', precio: 300, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: prod1, categoria:'tecnologia'},
@@ -17,11 +19,13 @@ export default function Catalogo() {
     { id:2 , nombre: 'iPhone SE', precio: 400, descripcion: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', img: prod3, categoria:'tecnologia'},
     { id:3 , nombre: 'iPhone 12', precio: 1000, descripcion: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.', img: prod4, categoria:'tecnologia'},
     { id:4 , nombre: 'test_item', precio: 8000, descripcion: 'test_desc', img: prod1, categoria:'tecnologia'},
-    { id:5 , nombre: 'test_item', precio: 8000, descripcion: 'test_desc', img: prod1, categoria:'tecnologia'},
+    { id:5 , nombre: 'test_item', precio: 8000, descripcion: 'test_desc', img: prod1, categoria:'Comida'},
     { id:6 , nombre: 'test_item', precio: 8000, descripcion: 'test_desc', img: prod1, categoria:'tecnologia'},
     { id:7 , nombre: 'test_item', precio: 8000, descripcion: 'test_desc', img: prod1, categoria:'tecnologia'},
     { id:8 , nombre: 'test_item', precio: 8000, descripcion: 'test_desc', img: prod1, categoria:'tecnologia'},
   ]);
+
+ 
 
   function setCards() {
     const row = [];
@@ -32,17 +36,35 @@ export default function Catalogo() {
       const img = products[i].img;
       const descripcion = products[i].descripcion;
       row.push(
+        
         <Card id={id} imgSrc={img} titulo={titulo} precio={precio} descripcion={descripcion}/>
       );
     }
     return row;
   }
 
+  const [searchTerm,setSearchTerm] = useState('')
   return (
     <>
     <CHeader/>
     <div className="catalogo-page">
-      <div className="spacer_top"></div>
+      <div className="spacer_top">
+        
+      </div>
+
+
+      <input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
+        {products.filter((val) => {
+          if (searchTerm ==""){
+            return setCards(val)
+          } else if (val.nombre.toLowerCase().includes(searchTerm.toLocaleLowerCase())){
+            return val
+          }
+        }).map((val,key) => {
+          return <div> {val.nombre}</div>;
+        })}
+
+
         <div id="features">
           <div className="card card-wide">
             <h5 className="card-header">Featured</h5>
@@ -54,6 +76,8 @@ export default function Catalogo() {
           </div>
         </div>
         
+       
+          
         <div className="catalogo">
           {setCards()}
         </div>      
