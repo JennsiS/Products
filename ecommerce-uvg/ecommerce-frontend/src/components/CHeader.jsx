5b849b350c120c191f7886bfd0d27d53881b3ad3
Javier Salazar <javierandre117@gmail.com>
2022-05-22 23:49:25 -0600

    Styling the project

--- ecommerce-uvg/ecommerce-frontend/src/components/CHeader.jsx
+++ ecommerce-uvg/ecommerce-frontend/src/components/CHeader.jsx
@@ -1,47 +1,25 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
-import "../styles/CHeader.scss";
-import logo from "../imgs/logo.png";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { FaGripHorizontal } from "react-icons/fa";
+import React from "react"
+import "../styles/CHeader.scss"
+import logo from "../imgs/logo.png"
+import { FaShoppingCart, FaStar } from 'react-icons/fa';
 
-export default class CHeader extends React.Component {
-  render() {
+export default function CHeader(props) {
     return (
       <header id="catalogo_header">
         <nav className="navver">
           <img id="catalogo_logo" src={logo} alt="" />
           <div id="spacer_div" />
           <ul className="nav_list">
-            <button type="button" id="nav_button">
-              {" "}
-              Home{" "}
-            </button>
-            <button type="button" id="nav_button">
-              {" "}
-              Browse{" "}
-            </button>
-            <button type="button" id="nav_button">
-              {" "}
-              Menu{" "}
-            </button>
-            <button type="button" id="nav_button">
-              {" "}
-              Products{" "}
-            </button>
+            <button type="button" id="nav_button"> Home </button>
+            <button type="button" id="nav_button"> Browse </button>
           </ul>
           <div className="__bar" />
           <ul className="nav_list">
-            <button type="button" id="nav_button">
-              {" "}
-              Contact{" "}
-            </button>
+            <button type="button" id="nav_button"> Contact </button>
           </ul>
-          <a href="#" className="btn btn-primary" id="btn-menu">
-            <FaGripHorizontal />
-          </a>
+          <a href="#ref" className="btn btn-primary iconHeader" id="btn-cart-header"><FaShoppingCart/></a>
+          <a href="#ref" className="btn btn-primary iconHeader" id="btn-star-header"><FaStar/></a>
         </nav>
       </header>
     );
-  }
 }
--- ecommerce-uvg/ecommerce-frontend/src/components/Catalogo.jsx
+++ ecommerce-uvg/ecommerce-frontend/src/components/Catalogo.jsx
@@ -1,167 +1,140 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import { React, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/Catalogo.scss";
-import "../styles/Cards.scss";
-import { FaCartPlus, FaStar } from "react-icons/fa";
-import CHeader from "./CHeader";
+import {React, useState} from "react";
+import '../styles/Catalogo.scss'
+import { FaCartPlus} from 'react-icons/fa';
+import CHeader from './CHeader';
+import Card from './ProductCard';
 
-import prod1 from "../imgs/phone_1.jpg";
+import prod1 from "../imgs/prod_1.png"
+import prod2 from "../imgs/prod_2.png"
+import prod3 from "../imgs/prod_3.png"
+import prod4 from "../imgs/prod_4.png"
+
 
 export default function Catalogo() {
   const [products, setProducts] = useState([
-    {
-      id: 0,
-      nombre: "phone1",
-      precio: 7000,
-      descripcion: "Phone 1",
-      img: prod1,
-      categoria: "tecnologia",
-    },
-    {
-      id: 1,
-      nombre: "phone2",
-      precio: 8000,
-      descripcion: "Phone 2",
-      img: prod1,
-      categoria: "tecnologia",
-    },
+    { id:"0" , nombre: 'Samsung S5', precio: 300, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', img: prod1, categoria:'tecnologia'},
+    { id:"1" , nombre: 'Samsung S4', precio: 250, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', img: prod2, categoria:'Phones'},
+    { id:"2" , nombre: 'iPhone SE', precio: 400, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', img: prod3, categoria:'Phones'},
+    { id:"3" , nombre: 'iPhone 12', precio: 1000, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', img: prod4, categoria:'tecnologia'},
+    { id:"4" , nombre: 'test_item', precio: 8000, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', img: prod1, categoria:'Phones'},
+    { id:"5" , nombre: 'test_item', precio: 8000, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', img: prod1, categoria:'Phones'},
+    { id:"6" , nombre: 'test_item', precio: 8000, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', img: prod1, categoria:'tecnologia'},
+    { id:"7" , nombre: 'test_item', precio: 8000, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', img: prod1, categoria:'tecnologia'},
+    { id:"8" , nombre: 'test_item', precio: 8000, descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.', img: prod1, categoria:'tecnologia'},
   ]);
+  const [listCategories, updateCategories] = useState(["All","Laptops","Phones","tecnologia"]);
+  const [displayProducts, updateDisplayProducts] = useState([...products]);
+
+  const [search,setSearch] = useState('');
+
+ function setCategories(){
+   const item = [];
+   for (let i=0; i<listCategories.length; i+=1){
+    item.push(
+      <li>
+        <a className="dropdown-item" href="#f" onClick={ 
+          function() {
+             if (listCategories[i]==="All"){
+               let category = products;
+               updateDisplayProducts([...category]);
+             }else{
+               let category = products.filter(products => products.categoria === listCategories[i])
+               updateDisplayProducts([...category])
+             }
+            }
+          }>{listCategories[i]}</a>
+      </li>
+    );
+   }
+   return item;
+ }
+
+  function setCards() {
+    const row = [];
+    for (let i = 0; i < displayProducts.length-1; i += 1) {
+      const id = displayProducts[i].id;
+      const titulo = displayProducts[i].nombre;
+      const precio = displayProducts[i].precio;
+      const img = displayProducts[i].img;
+      const descripcion = displayProducts[i].descripcion;
+      row.push(
+        
+        <Card id={id} imgSrc={img} titulo={titulo} precio={precio} descripcion={descripcion}/>
+      );
+    }
+    return row;
+  }
+
+
+  function setCardsBySearch(){
+    const row = [];
+    products.map((item) => {
+      if (search !== '' && item.nombre.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+        row.push(<Card id={item.id} imgSrc={item.img} titulo={item.nombre} precio={item.precio} descripcion={item.descripcion}/>)
+      }
+      else if (search === '') row.push(<Card id={item.id} imgSrc={item.img} titulo={item.nombre} precio={item.precio} descripcion={item.descripcion}/>)
+    })
+    return row;
+  }
+
+  const searchFilterFunction = (text) => {
+    setSearch(text);
+  }
+
+  function printfunction (){
+    if (search === ''){
+      return setCards()
+    }else {
+      return setCardsBySearch()
+    }
+  }
+  
   return (
     <>
-      <CHeader />
-      <div className="catalogo-page">
-        <div className="spacer_top"></div>
-        <div className="not" id="features">
-          <div class="card card-wide">
-            <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-              <h5 class="card-title">Peraphone S2</h5>
-              <p class="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                <FaCartPlus />
-              </a>
-            </div>
-          </div>
-        </div>
+    <CHeader/>
+    <div className="catalogo-page">
+      <div className="spacer_top">
+      </div>
 
-        <div className="catalogo">
-          <div className="card" alt="prueba">
-            <img className="card-img-top" src={products[0].img} alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{products[0].nombre}</h5>
-              <h5 className="card-title">{"$" + products[0].precio}</h5>
-              <p className="card-text">{products[0].descripcion}</p>
-              <a href="#" className="btn btn-primary">
-                <FaCartPlus />
-              </a>
-              <a href="#" className="btn btn-primary" id="btn-star">
-                <FaStar />
-              </a>
-            </div>
+      <div id="gridded_div">
+        <div className="class_left">
+          
+        </div>
+        <div className="class_center">
+          <input className="search_bar" 
+          type="text" 
+          placeholder="Search here ..." 
+          onChange={(text) => searchFilterFunction(text.target.value)}/>
+          
+        </div>
+        <div className="class_right">
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Categories
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              {setCategories()}
+            </ul>
           </div>
-          <div className="card" alt="prueba">
-            <img className="card-img-top" src={products[0].img} alt="" />
+        </div>
+      </div>
+
+        <div className="card" id="features">
+          <div className="card card-wide">
+            <h5 className="card-header">Featured</h5>
             <div className="card-body">
-              <h5 className="card-title">{products[0].nombre}</h5>
-              <h5 className="card-title">{"$" + products[0].precio}</h5>
-              <p className="card-text">{products[0].descripcion}</p>
-              <a href="#" className="btn btn-primary">
-                <FaCartPlus />
-              </a>
-              <a href="#" className="btn btn-primary" id="btn-star">
-                <FaStar />
-              </a>
-            </div>
-          </div>
-          <div className="card" alt="prueba">
-            <img className="card-img-top" src={products[0].img} alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{products[0].nombre}</h5>
-              <h5 className="card-title">{"$" + products[0].precio}</h5>
-              <p className="card-text">{products[0].descripcion}</p>
-              <a href="#" className="btn btn-primary">
-                <FaCartPlus />
-              </a>
-              <a href="#" className="btn btn-primary" id="btn-star">
-                <FaStar />
-              </a>
-            </div>
-          </div>
-          <div className="card" alt="prueba">
-            <img className="card-img-top" src={products[0].img} alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{products[0].nombre}</h5>
-              <h5 className="card-title">{"$" + products[0].precio}</h5>
-              <p className="card-text">{products[0].descripcion}</p>
-              <a href="#" className="btn btn-primary">
-                <FaCartPlus />
-              </a>
-              <a href="#" className="btn btn-primary" id="btn-star">
-                <FaStar />
-              </a>
-            </div>
-          </div>
-          <div className="card" alt="prueba">
-            <img className="card-img-top" src={products[0].img} alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{products[0].nombre}</h5>
-              <h5 className="card-title">{"$" + products[0].precio}</h5>
-              <p className="card-text">{products[0].descripcion}</p>
-              <a href="#" className="btn btn-primary">
-                <FaCartPlus />
-              </a>
-              <a href="#" className="btn btn-primary" id="btn-star">
-                <FaStar />
-              </a>
-            </div>
-          </div>
-          <div className="card" alt="prueba">
-            <img className="card-img-top" src={products[0].img} alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{products[0].nombre}</h5>
-              <h5 className="card-title">{"$" + products[0].precio}</h5>
-              <p className="card-text">{products[0].descripcion}</p>
-              <a href="#" className="btn btn-primary">
-                <FaCartPlus />
-              </a>
-              <a href="#" className="btn btn-primary" id="btn-star">
-                <FaStar />
-              </a>
-            </div>
-          </div>
-          <div className="card" alt="prueba">
-            <img className="card-img-top" src={products[0].img} alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{products[0].nombre}</h5>
-              <h5 className="card-title">{"$" + products[0].precio}</h5>
-              <p className="card-text">{products[0].descripcion}</p>
-              <a href="#" className="btn btn-primary">
-                <FaCartPlus />
-              </a>
-              <a href="#" className="btn btn-primary" id="btn-star">
-                <FaStar />
-              </a>
-            </div>
-          </div>
-          <div className="card" alt="prueba">
-            <img className="card-img-top" src={products[0].img} alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{products[0].nombre}</h5>
-              <h5 className="card-title">{"$" + products[0].precio}</h5>
-              <p className="card-text">{products[0].descripcion}</p>
-              <a href="#" className="btn btn-primary">
-                <FaCartPlus />
-              </a>
-              <a href="#" className="btn btn-primary" id="btn-star">
-                <FaStar />
-              </a>
+              <h5 className="card-title">Peraphone S2</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#ref" className="btn btn-primary"><FaCartPlus/></a>
             </div>
           </div>
         </div>
+        
+       
+          
+        <div className="catalogo">
+        {printfunction()}
+        </div>      
       </div>
     </>
   );
